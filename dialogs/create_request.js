26954cf9b4eb4b78b90d9ce0ded2759bd108508c
DixitaJain <dixita.jain@metacube.com>
2019-12-09 15:35:42 +0530

    message trails

--- dialogs/create_request.js
+++ dialogs/create_request.js
@@ -8,5 +8,28 @@ module.exports = controller => {
     convo.ask('What is your name?', async(response, convo, bot) => {
         console.log(`user name is ${ response }`);
     }, 'name');
+
+    convo.addMessage('Awesome {{vars.name}}!', 'favorite_color');
+    convo.addQuestion('Now, what is your favorite color?', async(response, convo, bot) => {
+        console.log(`user favorite color is ${ response }`);
+    },'color', 'favorite_color');
+
+    convo.addAction('confirmation' ,'favorite_color');
+
+    convo.addQuestion('Your name is {{vars.name}} and your favorite color is {{vars.color}}. Is that right?', [
+        {
+            pattern: 'no',
+            handler: async(response, convo, bot) => {
+                // if user says no, go back to favorite color.
+                await convo.gotoThread('favorite_color');
+            }
+        },
+        {
+            default: true,
+            handler: async(response, convo, bot) => {
+                console.dir(convo);
+            }
+        }
+    ], 'confirm', 'confirmation');
     controller.addDialog(convo);
 }
