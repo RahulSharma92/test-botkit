96977a461eda51865404e291b4bdfcc5705b688a
DixitaJain <dixita.jain@metacube.com>
2019-12-09 15:27:49 +0530

    message trails

--- dialogs/create_request.js
+++ dialogs/create_request.js
@@ -3,12 +3,39 @@ const connFactory = require('../util/connection-factory');
 
 module.exports = controller => {
     let convo = new BotkitConversation('create_request', controller);
+    convo.say('Howdy!');
 
-    convo.addMessage({
-        text: ``,
-        action: 'default'
-    }, '');
+    convo.ask('What is your name?', async(response, convo, bot) => {
+        console.log(`user name is ${ response }`);
+    }, 'name');
 
+    convo.addAction('favorite_color');
+
+    convo.addMessage('Awesome {{vars.name}}!', 'favorite_color');
+    convo.addQuestion('Now, what is your favorite color?', async(response, convo, bot) => {
+        console.log(`user favorite color is ${ response }`);
+    },'color', 'favorite_color');
+
+    convo.addAction('confirmation' ,'favorite_color');
+
+    // do a simple conditional branch looking for user to say "no"
+    convo.addQuestion('Your name is {{vars.name}} and your favorite color is {{vars.color}}. Is that right?', [
+        {
+            pattern: 'no',
+            handler: async(response, convo, bot) => {
+                // if user says no, go back to favorite color.
+                await convo.gotoThread('favorite_color');
+            }
+        },
+        {
+            default: true,
+            handler: async(response, convo, bot) => {
+                convo.addQuestion('Now, Do you like Singing?', async(response, convo, bot) => {
+                    console.log(`user Singing ${ response }`);
+                },'singing', 'default');
+            }
+        }
+    ], 'confirm', 'confirmation');
     convo.addQuestion('Please Enter the Account Name u want to create request for', [
         {
             default: true,
--- listeners/basic.js
+++ listeners/basic.js
@@ -36,40 +36,7 @@ module.exports = controller => {
                 } else if (message.intent === 'create_request') {
                     await bot.beginDialog('create_request');
                 } else {
-                    await bot.reply(message, 'hello');
-                    convo.say('Howdy!');
-
-                    convo.ask('What is your name?', async(response, convo, bot) => {
-                        console.log(`user name is ${ response }`);
-                    }, 'name');
-
-                    convo.addAction('favorite_color');
-
-                    convo.addMessage('Awesome {{vars.name}}!', 'favorite_color');
-                    convo.addQuestion('Now, what is your favorite color?', async(response, convo, bot) => {
-                        console.log(`user favorite color is ${ response }`);
-                    },'color', 'favorite_color');
-
-                    convo.addAction('confirmation' ,'favorite_color');
-
-                    // do a simple conditional branch looking for user to say "no"
-                    convo.addQuestion('Your name is {{vars.name}} and your favorite color is {{vars.color}}. Is that right?', [
-                        {
-                            pattern: 'no',
-                            handler: async(response, convo, bot) => {
-                                // if user says no, go back to favorite color.
-                                await convo.gotoThread('favorite_color');
-                            }
-                        },
-                        {
-                            default: true,
-                            handler: async(response, convo, bot) => {
-                                convo.addQuestion('Now, Do you like Singing?', async(response, convo, bot) => {
-                                    console.log(`user Singing ${ response }`);
-                                },'singing', '');
-                            }
-                        }
-                    ], 'confirm', 'confirmation');
+                    await bot.beginDialog('create_request');
                 }
             } catch (err) {
                 logger.log(err);
