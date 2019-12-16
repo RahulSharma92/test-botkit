2d2ca04ba8b9a11b8389512eec8535bc2ebacd42
DixitaJain <dixita.jain@metacube.com>
2019-12-16 18:37:55 +0530

    request POC

--- util/refedge.js
+++ util/refedge.js
@@ -1,5 +1,5 @@
 const logger = require('../util/logger');
-module.exports.getAccounts = getAccounts;
+
 module.exports = {
     saveTeamId: (conn, teamData) => {
         conn.apex.post('/rebot', teamData, (err, res) => {
@@ -8,40 +8,41 @@ module.exports = {
                 logger.log(err);
             }
         });
-    }
-};
-async function getAccounts(conn, accName) {
-    console.log('accName :: ' + accName);
-    if (accName == '' || accName == null) {
-        return 'false';
-    } else {
-        conn.apex.get('/rebot/' + accName , accName, (err, res) => {
-            var val = [];
-            if (err) {
-                logger.log(err);
-            }
-            if (res) {
-                if (res == 'false') {
-                    return null;
-                } else {
-                    res = JSON.parse(res);
-                    Object.keys(res).forEach(function(k){
-                        console.log(k + ' - ' + res[k]);
-                        var entry = {
-                            "text": {
-                                "type": "plain_text",
-                                "text": res[k]
-                            },
-                            "value": k
-                        }
-                        val.push(entry);
-                    });
-                    console.log('---------******');
-                    console.log(val);
-                    return val;
+    },
+    getAccounts: async (conn, accName) => {
+        console.log('accName :: ' + accName);
+        if (accName == '' || accName == null) {
+            return 'false';
+        } else {
+            conn.apex.get('/rebot/' + accName , accName, (err, res) => {
+                var val = [];
+                if (err) {
+                    logger.log(err);
                 }
-            }
-            return val;
-        });
+                if (res) {
+                    if (res == 'false') {
+                        return null;
+                    } else {
+                        res = JSON.parse(res);
+                        Object.keys(res).forEach(function(k){
+                            console.log(k + ' - ' + res[k]);
+                            var entry = {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": res[k]
+                                },
+                                "value": k
+                            }
+                            val.push(entry);
+                        });
+                        console.log('---------******');
+                        console.log(val);
+                        return val;
+                    }
+                }
+                return val;
+            });
+        }
     }
-}
+
+};
