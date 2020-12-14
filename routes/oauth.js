a15416ad69c6ffd91cca5d4cf428624a3bc7b805
Rahul Sharma <38658846+RahulSharma92@users.noreply.github.com>
2020-12-14 22:43:37 +0530

    trying older way of getting v2 token

--- routes/oauth.js
+++ routes/oauth.js
@@ -9,12 +9,41 @@ module.exports = controller => {
         try {
             console.log('-----/oauth/req-----')
             console.dir(controller.adapter);
+            let botInstance = controller.adapter.middlewares.spawn({});
+            let options = {
+                client_id: controller.adapter.options.clientId,
+                client_secret: controller.adapter.options.clientSecret,
+                code: req.query.code
+            };
+            botInstance.api.oauth.v2.access(options, (err, auth) => {
+                console.log('!----- inside function ---------!');
+                
+                if (err) {
+                    console.dir(err);
+                    res.status(401);
+                    return res.redirect('/auth-failed.html');
+                }
 
-            const authData = await controller.adapter.validateOauthCode(req.query.code);
-            console.log('-----/authData/-----')
+                botInstance.api.auth.test({ token: auth.access_token }, (err, identity) => {
+
+                    if (err) {
+                        res.status(401);
+                        return res.redirect('/auth-failed.html');
+                    }
+                    console.log('........success............');
+                    auth.identity = identity;
+                    controller.trigger('oauth_success', [auth]);
+                    res.redirect(`https://slack.com/app_redirect?app=${process.env.SLACK_APP_ID}`);
+                });
+            });
+
+
+
+            //const authData = await controller.adapter.validateOauthCode(req.query.code);
+            /* console.log('-----/authData/-----')
             console.dir(req.query)
             controller.trigger('oauth_success', authData);
-            res.redirect(`https://slack.com/app_redirect?app=${process.env.SLACK_APP_ID}`);
+            res.redirect(`https://slack.com/app_redirect?app=${process.env.SLACK_APP_ID}`); */
         } catch (err) {
             console.error('OAUTH ERROR: ', err);
             res.status(401);
